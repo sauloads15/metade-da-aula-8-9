01286ba0569f9fa4fab1391e85c6e79618e4295c
sauloads15 <saulosmithads15@gmail.com>
2024-08-08 12:33:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// questao 3: 
+//const novoElemento = document.createElement('div');
+// novoElemento.textContent = 'FEC NO G3!';
+        
+// document.body.appendChild(novoElemento);
+
+//Questao 4:
+// function trocarImagem() {
+//   // Muda a fonte da imagem
+//   document.getElementById('minhaImagem').src = 'imagem-nova.jpg';}
+// Questao 5:
+// let selecionar = document.getElementById('#oi');
+// let selecionarFilho = document.querySelector('.ola');
+// selecionarFilho.innerText = 'saulo';
+
+// function trocarText
+
+// const input = document.getElementById('meuInput');
+// input.addEventListener('input', (event) => {
+//   console.log(`Texto digitado: ${event.target.value}`);
+// });
+// const novoDiv= document.createElement('div');
+// novoDiv.textContent= 'Este é um novo elemento div.';
+// novoDiv.style.backgroundColor = 'lightblue';
+// novoDiv.style.padding= '10px';
+// novoDiv.style.border= '1px solid blue';
+
+// const conteudo= document.getElementById('conteudo');
+// conteudo.appendChild(novoDiv);
+// function criar(){
+//   let box= document.querySelector('.box');
+//   box.style.width= '100px';
+//   box.style.height='100px';
+//   box.style.backgroundColor= '#f00';
+
+// }
+
+
+function estilizar Paragrafo() { const cor document.getElementById('corInput').value;
+const paragrafo document.getElementById('paragrafo"); paragrafo.style.color = cor;
+cor.value 
+}
+const estilizarBotao document.getElementById('estilizarBotao");
+   estilizarBotao.addEventListener('click', estilizar Paragrafo);
+
+
+
+
+
+
+
+
+
